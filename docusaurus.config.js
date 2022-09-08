255a9a2c8bebac8502df0ec9b9f2eb0408f92b11
TMN <t.martin@adsoftware.fr>
2022-09-08 20:28:00 +0200

    update url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,7 +36,7 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/mlfcnt/docuwiki",
+          editUrl: "https://github.com/mlfcnt/docuwiki/blob/master",
         },
         blog: {
           showReadingTime: true,
